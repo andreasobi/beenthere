a42425c6ab7119c02b78253a2b54a927b85de63c
andiobi <andreas.oberrauter@gmail.com>
2024-02-03 19:40:37 +0100

    auth bugfix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,34 +10,36 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_SECRET
         })
     ],
-    async session({session}) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-        session.user.id = sessionUser._id.toString();
-        return session;
-    },
-    async signIn({profile}) {
-        try {
-            await connectToDB();
-
-            // check if user already exists
-            const userExists = await User.findOne({
-                email: profile.email
-            });
-            // if user doesn't exist, create new
-            if(!userExists){
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ","").toLowerCase(),
-                    image: profile.image
-                })
+    callbacks: {
+        async session({session}) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
+            session.user.id = sessionUser._id.toString();
+            return session;
+        },
+        async signIn({profile}) {
+            try {
+                await connectToDB();
+    
+                // check if user already exists
+                const userExists = await User.findOne({
+                    email: profile.email
+                });
+                // if user doesn't exist, create new
+                if(!userExists){
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ","").toLowerCase(),
+                        image: profile.image
+                    })
+                }
+                return true;
+    
+            } catch (error) {
+                console.log(error);
+                return false;
             }
-            return true;
-
-        } catch (error) {
-            console.log(error);
-            return false;
         }
     }
 })
